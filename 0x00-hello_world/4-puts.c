576d4fa7b080162621f6c162fd70aac5e5da16a3
Ella-Emefa <ellaemefafiagbe@gmail.com>
2022-12-11 14:17:55 -0800

    a new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * Main: Entry point
+ * Main -  entry point
  * Return: A string
  */
 int main(void)
-{puts("Programming is like building a multilingual puzzle\n");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
