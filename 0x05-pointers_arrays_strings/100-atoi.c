feac7667c9c3cbc207c136e8791942608bcc4feb
Ella-Emefa <ellaemefafiagbe@gmail.com>
2023-01-01 18:56:12 -0800

    Convert string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,23 @@
 #include "main.h"
+
 /**
   * _atoi - converts a string to an integer
   * @s: string
   * Return: integer
   */
+
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
 		while (*(s + count) != '\0')
 		{
 			if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 				break;
+
 			if (*(s + count) == '-')
 				pn *= -1;
+
 			if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 			{
 				if (size > 0)
@@ -21,6 +26,7 @@ int _atoi(char *s)
 			}
 			count++;
 		}
+
 	for (i = count - size; i < count; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int _atoi(char *);
+
+/**
+  * main - check the code
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int n;
+
+	n = _atoi("4");
+	printf("%d\n", n);
+	return (0);
+}
